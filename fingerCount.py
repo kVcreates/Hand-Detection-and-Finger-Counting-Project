7d16eaa8509fc2c8ee442de6bb9908fc18ddba7e
Kumar Vaibhav <kumarvaibhav460@gmail.com>
2023-10-14 14:11:47 +0530

    Commiting after so long

--- fingerCount.py
+++ fingerCount.py
@@ -8,8 +8,8 @@ while True:
     ret, img = video.read()
     img = cv.flip(img, 1)
     hand = detector.findHands(img, draw = False)
-
-    fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python\OpenCv\Hand Detection and Finger Counting Project\Finger Images\0fing.jpg")
+    
+    fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python Prac\OpenCv\Hand Detection and Finger Counting Project\Finger Images\0fing.jpg")
 
     if hand:
         lmlist = hand[0]
@@ -17,22 +17,22 @@ while True:
             fingersup = detector.fingersUp(lmlist)
             
             if fingersup == [1,0,0,0,0]:
-                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python\OpenCv\Hand Detection and Finger Counting Project\Finger Images\thumbsup.jpg")
+                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python Prac\OpenCv\Hand Detection and Finger Counting Project\Finger Images\thumbsup.jpg")
                 cv.putText(img, 'Thumb', (50,400), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
             if fingersup.count(1) == 1 and fingersup != [1,0,0,0,0]:
-                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python\OpenCv\Hand Detection and Finger Counting Project\Finger Images\1fing.jpg")
+                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python Prac\OpenCv\Hand Detection and Finger Counting Project\Finger Images\1fing.jpg")
                 cv.putText(img, '1 Finger', (50,400), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
             if fingersup.count(1) == 2:
-                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python\OpenCv\Hand Detection and Finger Counting Project\Finger Images\2fing.jpg")
+                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python Prac\OpenCv\Hand Detection and Finger Counting Project\Finger Images\2fing.jpg")
                 cv.putText(img, '2 Fingers', (50,400), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
             if fingersup.count(1) == 3:
-                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python\OpenCv\Hand Detection and Finger Counting Project\Finger Images\3fing.jpg")
+                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python Prac\OpenCv\Hand Detection and Finger Counting Project\Finger Images\3fing.jpg")
                 cv.putText(img, '3 Fingers', (50,400), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
             if fingersup.count(1) == 4:
-                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python\OpenCv\Hand Detection and Finger Counting Project\Finger Images\4fing.jpg")
+                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python Prac\OpenCv\Hand Detection and Finger Counting Project\Finger Images\4fing.jpg")
                 cv.putText(img, '4 Fingers', (50,400), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
             if fingersup.count(1) == 5:
-                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python\OpenCv\Hand Detection and Finger Counting Project\Finger Images\5fing.jpg")
+                fing = cv.imread(r"C:\Users\HP\OneDrive\Desktop\Python Prac\OpenCv\Hand Detection and Finger Counting Project\Finger Images\5fing.jpg")
                 cv.putText(img, '5 Fingers', (50,400), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
     
     fing = cv.resize(fing, (220, 280))
